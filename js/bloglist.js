94d050078536f4eaa3b6ca1d9bd196fd33f37317
adarsh <aksking99@gmail.com>
2019-07-12 20:11:55 +0530

    2nd page with trash icon and ... icon

--- js/bloglist.js
+++ js/bloglist.js
@@ -1,1 +1,24 @@
 /* del() activates the modal that deletes the blog posts */
+function del() {
+    // Fetch the modal
+    var modal = document.getElementById("del");
+    // Display the modal to the user
+    modal.style.display = "block";
+  
+    // Hides the modal if user clicks else where on the display
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+  
+    }
+  }
+  
+  
+  /* hid() hides the modal that deletes the blog posts */
+  function hid() {
+    // Hides the modal when user click on the 'No' button
+    var btn = document.getElementsByClassName('modal')[2];
+    btn.style.display = "none";
+  
+  }
